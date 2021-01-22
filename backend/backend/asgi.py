46c2cd3765e8e51a01c25e10397ad21109ec3041
WillYip716 <william.yip.716@gmail.com>
2021-01-21 19:01:53 -0700

    basic models

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for score_predictor project.
+ASGI config for backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'score_predictor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""score_predictor URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
