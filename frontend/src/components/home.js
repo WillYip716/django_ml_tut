5aa652d2c9c4d12dc6678751a275e3be61d5d668
WillYip716 <william.yip.716@gmail.com>
2021-01-28 19:32:51 -0700

    axios server basic service requests

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-//import axios from 'axios';
-import {Link} from 'react-router-dom';
+import axios from 'axios';
+//import {Link} from 'react-router-dom';
 
 
 class Home extends React.Component{
@@ -15,13 +15,42 @@ class Home extends React.Component{
 
     componentDidMount() {
 
+        axios.get('/api/posts')
+        .then(res => {
+            const posts = res.data;
+            this.setState((state) => ({
+                loading: false,
+                posts: posts,
+            }));  
+        })
+
     }
 
     render(){
+        let l = this.state.posts.length;
+        let items = [];
+
+        for (var i = 0; i <l;i++) {
+            items.push(
+                <div key={this.state.posts[i].id}>
+                    <h3>{unescape(this.state.posts[i].title)}</h3>
+                    <p>{unescape(this.state.posts[i].message)}</p>
+                </div>
+            )
+        }
+
+
 
         return(
             <div>
-                <h1>Home</h1> 
+                {this.state.loading        
+                ? <h1>Hello i am loading</h1>
+                :<div>
+                    <h1 style={{textAlign:"center"}}>LATEST POSTS</h1>
+                    <hr/>
+                    {items}
+                </div>
+            }
             </div>
         )
     }  
