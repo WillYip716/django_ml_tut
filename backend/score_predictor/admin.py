dca0f12a78cf2c9fd59212f39d68c81c4f1caacf
WillYip716 <william.yip.716@gmail.com>
2021-02-28 20:49:58 -0700

    reset of app with new tutorial

--- backend/score_predictor/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post # add this
-
-class PostAdmin(admin.ModelAdmin):  # add this
-  list_display = ('title', 'message', 'date') # add this
-
-# Register your models here.
-admin.site.register(Post, PostAdmin) # add this
